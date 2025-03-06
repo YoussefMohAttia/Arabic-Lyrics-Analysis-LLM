408653af373479ba42aa9f0d74fabe3380076a5e
Mazen-Elsayed <142096839+Mazen-Elsayed@users.noreply.github.com>
2025-03-06 16:17:12 +0200

    Week 2: Preprocessing Tasks

---
+++ add_python_paths.bat
@@ -0,0 +1,5 @@
+@echo off
+setx PATH "%PATH%;C:\Users\mazen\AppData\Roaming\Python\Python312\Scripts"
+echo Python Scripts path has been added to your PATH environment variable.
+echo Please restart your command prompt for the changes to take effect.
+pause
